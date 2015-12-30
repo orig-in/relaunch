ba53baa04db0b475eae571b1c20a93c16e4656e7
Matthias Cullmann <matthias.cullmann@baloise.ch>
2015-12-30 16:43:31 +0100

    launch script with several params

--- src/main/java/com/github/origin/relaunch/Relaunch.java
+++ src/main/java/com/github/origin/relaunch/Relaunch.java
@@ -4,6 +4,7 @@ import static java.lang.String.format;
 import static java.lang.Thread.sleep;
 import static java.nio.file.Files.move;
 import static java.nio.file.Paths.get;
+import static java.util.Arrays.asList;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,16 +15,25 @@ public class Relaunch {
 	public static void main(String[] args) throws InterruptedException, IOException {
 		String oldApp = args[0];
 		String newApp = args[1];
-		String launchScript = args[2];
-		System.out.println(format("Relaunching %s %s %s", oldApp, newApp, launchScript));
-		
+		String[] launchScript =  new String[args.length-2];
+		System.arraycopy(args, 2, launchScript, 0, launchScript.length);
+		System.out.println(format("Relaunching %s %s %s", oldApp, newApp, asList(launchScript)));
+		checkFile(oldApp);
+		checkFile(newApp);
 		while(!new File(oldApp).delete()) {
 			System.out.println(new Date() + " waiting to delete "+oldApp);
 			sleep(1000);
 		}
 		
 		move(get(newApp), get(oldApp));
-		new ProcessBuilder("cmd.exe", "/C", launchScript).start();
+		new ProcessBuilder(launchScript).start();
+	}
+
+	private static void checkFile(String oldApp) {
+		if(!new File(oldApp).exists()) {
+			System.out.println(oldApp + " does not exist");
+			System.exit(1);
+		}
 	}
 
 }
